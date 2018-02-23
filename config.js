13de3eb3cf474d631cf91f3213b5b43c151352d3
Krzysztof Ługowski <k.lugowski@yahoo.com>
2018-02-23 12:37:41 +0100

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '956963329274-la9c9sh16gphbcro4ou8uhhcir688ijt.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'aJsZDM2cGECJatQzQVfTd6iR',
+	'CALLBACK_URL': 'HTTP://localhost:3000/auth/google/callback'
+};
